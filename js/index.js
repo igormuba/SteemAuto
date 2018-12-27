19ef347b09e50a3f6bbacb2b79bee1a5c05bb555
igormuba <igorm-uba@hotmail.com>
2018-12-27 21:50:25 -0200

    styles

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
 var api = sc2.Initialize({
-    app: 'steemhere',
-    callbackURL: '127.0.0.1:5500/app.html/',
-    accessToken: 'access_token',
-    scope: ['vote']
-  });
+  app: 'steemhere',
+  callbackURL: 'http://localhost:5500/app/',
+  accessToken: 'access_token',
+  scope: ['vote', 'comment']
+});
 
-  var link = api.getLoginURL();
-  console.log(link)
